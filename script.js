2798915547e0f067d3669130115f4568156ac050
Arban Hossain <arbanhossain@gmail.com>
2022-12-03 19:34:40 +0600

    drawing optimizations, swap block only with adjacent

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let ctx = canvas.getContext('2d');
 
 // A point
 class Point {
-  constructor(h,w) {
+  constructor(h, w) {
     this.x = h;
     this.y = w;
   }
@@ -22,6 +22,26 @@ class Point {
 //
 // --------- GAME CONSTANTS ---------
 //
+
+// Debug flag
+const DEBUG = true;
+
+// Bounding box for canvas areas to draw/update
+const drawQueue = {
+  levelScore: {
+    toDraw: true,
+    rectSize: {
+      from: new Point(0, 0),
+      to: new Point(w/3, 60),
+    },
+  },
+  grid: {
+    toDraw: true,
+    rectSize: {
+      from: new Point()
+    }
+  },
+};
 
 // Game default block/sprite size in pixels
 const N = 32
@@ -35,7 +55,7 @@ const spriteCodes = {
 
 // Game State
 const gameState = {
-  gameWindowStart: new Point(w/128, h/128),
+  gameWindowStart: new Point(w / 128, h / 128),
   levelCount: 0,
   levelLayout: null,
   score: 0,
@@ -56,14 +76,14 @@ const drawLine = (ctx, p0, p1) => {
 // Draw a circle of given radius with given point
 const drawCircle = (ctx, point, radius) => {
   ctx.beginPath();
-  ctx.arc(point.x, point.y, radius, 0, 2*Math.PI);
+  ctx.arc(point.x, point.y, radius, 0, 2 * Math.PI);
   ctx.stroke();
 }
 
 // Draw a sprite at a given point with optional height and width arguments
 // Will default to NxN if not specified
 // Might not work as intended if the height and width are not consistent across all sprites
-const drawSprite = (ctx, imgSrc, pos, height=N, width=N) => {
+const drawSprite = (ctx, imgSrc, pos, height = N, width = N) => {
   let img = new Image();
   img.onload = () => {
     if (height && width) {
@@ -79,15 +99,26 @@ const drawSprite = (ctx, imgSrc, pos, height=N, width=N) => {
 // ---------- UTILITY FUNCTIONS ----------
 //
 
+// Get neighbors of a given cell
+const getNeighbors = (i, j) => {
+  return [
+    new Point(i-1, j),
+    new Point(i+1, j),
+    new Point(i, j-1),
+    new Point(i, j+1),
+  ]
+}
+
 // Generate a level with given size
 const getLevel = (n) => {
   arr = [];
-  for(let i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     arr.push([]); // Make a row
-    for(let j = 0; j < n; j++) {
+    for (let j = 0; j < n; j++) {
       // Put 0 and 1 randomly in the row
       let spriteType = Math.round(Math.random());
       arr[i][j] = {
+        _id: Math.round(Math.random()*1e+6),
         type: spriteType,
         pastType: null,
         src: spriteCodes[spriteType]
@@ -99,22 +130,23 @@ const getLevel = (n) => {
 
 // Return sprite type at position x, y
 const getSpriteAtPos = (x, y) => {
-  return gameState.levelLayout[Math.floor(y/N)][Math.floor(x/N)];
+  return gameState.levelLayout[Math.floor(y / N)][Math.floor(x / N)];
 }
 
 // Get the mouse position on the canvas AND decide what to do depending on the sprite selected
 const getCursorPosition = (canvas, event) => {
   // Get mouse position
   const rect = canvas.getBoundingClientRect();
-  let x = event.clientX - rect.left - gameState.gameWindowStart.x*N;
-  let y = event.clientY - rect.top - gameState.gameWindowStart.y*N;
+  let x = event.clientX - rect.left - gameState.gameWindowStart.x * N;
+  let y = event.clientY - rect.top - gameState.gameWindowStart.y * N;
 
   // Get sprite at mouse position
-  let sprite = gameState.levelLayout[Math.floor(y/N)][Math.floor(x/N)];
+  let sprite = gameState.levelLayout[Math.floor(y / N)][Math.floor(x / N)];
 
   if (sprite.type == 'selected') { // If the sprite is already selected, deselect it
-    
-    gameState.levelLayout[Math.floor(y/N)][Math.floor(x/N)] = {
+
+    gameState.levelLayout[Math.floor(y / N)][Math.floor(x / N)] = {
+      _id: sprite._id,
       type: sprite.pastType,
       pastType: null,
       src: spriteCodes[sprite.pastType]
@@ -124,28 +156,45 @@ const getCursorPosition = (canvas, event) => {
     gameState.selected = null;
 
   } else if (sprite.type == 0 || sprite.type == 1) { // If the sprite is not selected, and is of a selectable type
-    if(gameState.selected != null) { // and if there is already a selected sprite, swap that with this one
+    if (gameState.selected != null) { // and if there is already a selected sprite
       
-      gameState.levelLayout[gameState.selectedPosition.y][gameState.selectedPosition.x] = sprite;
+      let neighbors = getNeighbors(gameState.selectedPosition.y, gameState.selectedPosition.x); // get neighbors of the selected sprite
 
-      gameState.levelLayout[Math.floor(y/N)][Math.floor(x/N)] = {
-        type: gameState.selected.pastType,
-        pastType: null,
-        src: spriteCodes[gameState.selected.pastType]
-      };
+      for(let i = 0; i < neighbors.length; i++) { // loop over the neighbors
+        let point = neighbors[i];
+        if(point.x < 0 || point.y < 0 || point.x >= gameState.levelLayout.length || point.y >= gameState.levelLayout.length) {
+          continue; // if the neighbor indices are out of bounds, continue
+        }
+        if(gameState.levelLayout[point.x][point.y]._id == sprite._id) { // if the sprite is matched to be a neighbor
 
-      gameState.selectedPosition = null;
-      gameState.selected = null;
+          // swap the sprite with the selected one
+          gameState.levelLayout[gameState.selectedPosition.y][gameState.selectedPosition.x] = sprite;
+    
+          gameState.levelLayout[Math.floor(y / N)][Math.floor(x / N)] = {
+            _id: gameState.selected._id,
+            type: gameState.selected.pastType,
+            pastType: null,
+            src: spriteCodes[gameState.selected.pastType]
+          };
+    
+          gameState.selectedPosition = null;
+          gameState.selected = null;
+
+          break; // exit the loop
+        }
+      }
+
 
     } else { // and if there is no selected sprite, set this one as selected
-      
-      gameState.levelLayout[Math.floor(y/N)][Math.floor(x/N)] = {
+
+      gameState.levelLayout[Math.floor(y / N)][Math.floor(x / N)] = {
+        _id: sprite._id,
         type: 'selected',
         pastType: sprite.type,
         src: spriteCodes['selected']
       };
 
-      gameState.selectedPosition = new Point(Math.floor(x/N), Math.floor(y/N));
+      gameState.selectedPosition = new Point(Math.floor(x / N), Math.floor(y / N));
       gameState.selected = gameState.levelLayout[gameState.selectedPosition.y][gameState.selectedPosition.x];
 
     }
@@ -159,22 +208,33 @@ const getCursorPosition = (canvas, event) => {
 //
 
 const gameLoop = () => {
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if(gameState.levelLayout == null) {
+  if (gameState.levelLayout == null) {
     gameState.levelLayout = getLevel(10);
     gameState.levelCount++;
   }
-  
+
   // Draw the sprites in the level
-  for(let i = 0; i < gameState.levelLayout.length; i++) {
+  for (let i = 0; i < gameState.levelLayout.length; i++) {
     let y = i + gameState.gameWindowStart.y;
-    for(let j = 0; j < gameState.levelLayout[i].length; j++) {
+    for (let j = 0; j < gameState.levelLayout[i].length; j++) {
       let x = j + gameState.gameWindowStart.x;
-      drawSprite(ctx, spriteCodes[gameState.levelLayout[i][j].type], new Point(x*N, y*N), N-1, N-1);
+      drawSprite(ctx, spriteCodes[gameState.levelLayout[i][j].type], new Point(x * N, y * N), N - 1, N - 1);
     }
   }
-  
+
+  if (drawQueue.levelScore.toDraw) {
+    let rect = drawQueue.levelScore.rectSize;
+    ctx.clearRect(rect.from.x, rect.from.y, rect.to.x, rect.to.y);
+    ctx.font = '48px Arial';
+    ctx.fillText(`Level: ${gameState.levelCount}`, 10, 50);
+    drawQueue.levelScore.toDraw = false;
+  }
+
+  if (DEBUG) {
+
+  }
+
   // Get next frame
   window.requestAnimationFrame(gameLoop);
 }
@@ -186,6 +246,14 @@ const gameLoop = () => {
 // On click event listener
 canvas.addEventListener('mousedown', (e) => {
   getCursorPosition(canvas, e);
+})
+
+document.addEventListener('keydown', (e) => {
+  if (e.code == 'Space') {
+    gameState.levelCount++;
+    drawQueue.levelScore.toDraw = true;
+    //ctx.clearRect(0, 0, w, 100);
+  }
 })
 
 // Start getting frames
